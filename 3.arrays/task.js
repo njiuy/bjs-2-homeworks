92f1fd582b52795fa53bbace00a9446e1838eb66
njiuy <makeeva.sa2308@mail.ru>
2023-06-28 13:01:18 +0300

    :Реализовано третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+ if (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])) {
+    return true;
+ } else {
+    return false;
+ }
 }
-
+ 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).map(filteredUser => filteredUser.age)
+  .reduce((accumulator, age, index, userAgesArray) => accumulator + age / userAgesArray.length, 0);
+return result;
 }
