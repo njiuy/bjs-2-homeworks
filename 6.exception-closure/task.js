0492b01ae3c18f8f5aa755f7e73762d4013d707e
njiuy <makeeva.sa2308@mail.ru>
2023-07-03 13:35:46 +0300

    :Реализовано шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+    let result = Number.parseFloat(value) 
+        if(isNaN(result)) {
+            throw new Error("Невалидное значение");
+        } return result;
+    }
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch(Error) {
+        return(Error);
+    }
+} 
+
+class Triangle {
+    constructor(a,b,c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if(a + b < c || b + c < a || a + c < b) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a,b,c) {
+    try {
+        return new Triangle(a,b,c);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }, 
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
