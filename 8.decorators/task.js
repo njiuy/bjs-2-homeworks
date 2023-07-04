21beae361f0c1fd86cb8dac4796cbe5b58d94fc6
njiuy <makeeva.sa2308@mail.ru>
2023-07-04 12:46:52 +0300

    :Реализовано восьмое задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+    let cache = [];
+
+    function wrapper(...args) {     
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash ); 
+        if (objectInCache) { 
+            return "Из кэша: " + objectInCache.value;
+        }   
+        let result = func(...args); 
+        let resultObj = {
+          hash: hash, 
+          value: result 
+        };
+        cache.push(resultObj) ; 
+      
+        if (cache.length > 5) { 
+          cache.shift();  
+        }
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
+  }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timerId = null;
+  function resultFunction(...args) {
+    if(resultFunction.allCount === 0) {
+      func(...args);
+      resultFunction.count++;
+    }
+
+    resultFunction.allCount++;
+
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func(...args);
+      resultFunction.count++;
+    }, delay);
+  } 
+   resultFunction.count = 0;
+   resultFunction.allCount = 0;
+   
+   return resultFunction;
 }
+
